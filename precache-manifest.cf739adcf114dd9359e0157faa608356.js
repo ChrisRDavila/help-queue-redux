6151055c229504ab1869c5d8f2f356b09d004b01
ChrisRDavila <crossdavila@gmail.com>
2023-07-05 15:32:58 -0700

    Updates

---
+++ precache-manifest.cf739adcf114dd9359e0157faa608356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796a3d507c4a8661eaec33f92c4a93a7",
+    "url": "/ChrisRDavila/help-queue-redux.git/index.html"
+  },
+  {
+    "revision": "3c0634c697901ad7bac7",
+    "url": "/ChrisRDavila/help-queue-redux.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f95310a2c68d5c38e19",
+    "url": "/ChrisRDavila/help-queue-redux.git/static/js/2.7dad1d6a.chunk.js"
+  },
+  {
+    "revision": "3c0634c697901ad7bac7",
+    "url": "/ChrisRDavila/help-queue-redux.git/static/js/main.6ae25c1a.chunk.js"
+  },
+  {
+    "revision": "ef9574ea8426bfee24a0",
+    "url": "/ChrisRDavila/help-queue-redux.git/static/js/runtime-main.d7d90051.js"
+  }
+]);
